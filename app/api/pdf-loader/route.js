b7ff4cb758e5155fb275a8335dec5382e95ef0be
adriandevv <39228043+adriandevv@users.noreply.github.com>
2025-02-26 09:18:39 +0000

    Agregar funcionalidad para cargar y procesar archivos PDF, incluyendo la implementación de un endpoint API para extraer texto y dividirlo en fragmentos

---
+++ app/api/pdf-loader/route.js
@@ -0,0 +1,31 @@
+import { NextResponse } from "next/server";
+import { WebPDFLoader } from "@langchain/community/document_loaders/web/pdf";
+import { RecursiveCharacterTextSplitter } from "@langchain/textsplitters";
+
+
+const pdfURL='https://groovy-platypus-381.convex.cloud/api/storage/b5b184a3-8897-4ee8-b23c-7c848e38ddb2';
+export async function GET (req){
+    //1. Load the PDF from the URL
+    const response = await fetch(pdfURL);
+    const data = await response.blob();
+    const loader = new WebPDFLoader(data);
+    const docs = await loader.load();
+
+    let pdfTextContent = '';
+    docs.forEach(doc => {
+        pdfTextContent += doc.pageContent;
+    });
+
+    //2. split message into small chunks
+    const splitter = new RecursiveCharacterTextSplitter({
+        chunksize:100,
+        chunkOverlap: 20,
+    });
+    const output = await splitter.createDocuments([pdfTextContent]);
+    let splitterList = [];
+    output.forEach(doc => {
+        splitterList.push(doc.pageContent);
+    });
+    
+    return NextResponse.json({result: splitterList});
+}
